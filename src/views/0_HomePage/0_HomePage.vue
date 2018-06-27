a30e1acb6df4ab3f668a445169f3ef200b808c81
vicbergquist <victoria.bergquist@gmail.com>
2018-06-27 13:08:55 +0200

    routing link in nav

---
+++ src/views/0_HomePage/0_HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+      <h1>landing page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style lang="sass">
+
+  @import 'src/assets/styles/style-variables.sass'
+
+</style>
