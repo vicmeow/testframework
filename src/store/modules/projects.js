727625592bd0aa0b90e9c632962795c9129a2a99
vicbergquist <victoria.bergquist@gmail.com>
2018-07-04 14:51:05 +0200

    updated ID for projects mockdata

--- src/store/modules/projects.js
+++ src/store/modules/projects.js
@@ -3,9 +3,9 @@ const state = {
     {
       title: 'Project 1',
       type: 'project',
-      id: 1,
+      id: 12345,
       status: 1,
-      time: 'XX.XX.XXXX',
+      date: 'XX.XX.XXXX',
       description: 'This will be the description of this project.',
       data: {
         'test cases': 45,
@@ -16,9 +16,9 @@ const state = {
     {
       title: 'Project 2',
       type: 'project',
-      id: 2,
+      id: 65789,
       status: 0,
-      time: 'XX.XX.XXXX',
+      date: 'XX.XX.XXXX',
       description: 'This will be the description of this project.',
       data: {
         'test cases': 45,
